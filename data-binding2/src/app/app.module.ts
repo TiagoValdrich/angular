c07432acb347abc8661bc536ec1d79987a6c2ff6
Tiago José Valdrich <tiago.valdrich@hotmail.com>
2018-06-24 17:16:49 -0300

    Adding a two-way data binding testing component and a new module.

--- data-binding2/src/app/app.module.ts
+++ data-binding2/src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ModalComponent } from './modal/modal.component';
 import { EventBindingComponent } from './event-binding/event-binding.component';
+import { TwoWayDataBindingComponent } from './two-way-data-binding/two-way-data-binding.component';
+import { MeuFormModule } from './meu-form/meu-form.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     DataBindingComponent,
     ModalComponent,
-    EventBindingComponent
+    EventBindingComponent,
+    TwoWayDataBindingComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    FormsModule,
+    NgbModule.forRoot(),
+    MeuFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
