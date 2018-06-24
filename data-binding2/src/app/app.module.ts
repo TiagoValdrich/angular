613ae2899b422e085925c85c2e5fe4997b77261e
Tiago José Valdrich <tiago.valdrich@hotmail.com>
2018-06-24 14:21:25 -0300

    Adding the repositories of my test project

---
+++ data-binding2/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ModalComponent } from './modal/modal.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DataBindingComponent,
+    ModalComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgbModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
