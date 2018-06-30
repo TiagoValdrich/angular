42faa104e9122d90580ec153d6eb48be1c5d852e
Tiago José Valdrich <tiago.valdrich@hotmail.com>
2018-06-30 16:55:18 -0300

    Organizing the class Carro, and adding new classes: Moto and Veiculo, and implementing the heritage between Veiculo -> Carro, Moto and testing them

--- typescript/typescript/Carro.ts
+++ typescript/typescript/Carro.ts
@@ -1,27 +1,14 @@
-export default class Carro {
+import Veiculo from './Veiculo'
 
-    private modelo: string
+export default class Carro extends Veiculo {
+
     private numeroDePortas: number
-    private velocidade: number = 0
     // private velocidade: number = 0 // Podemos instanciar os atributos de uma classe dessa forma, ou seja, fora do método construtor.
 
-    constructor(modelo:string, numeroDePortas: number){
+    constructor(modelo: string, numeroDePortas: number){
+        super()
         this.modelo = modelo
         this.numeroDePortas = numeroDePortas
     }
 
-    public acelerar(): void {
-        this.velocidade = this.velocidade + 10
-    }
-
-    public parar(): void {
-        this.velocidade = 0;
-    }
-
-    public velocidadeAtual(): number {
-        return this.velocidade
-    }
-
 }
-
-export let qualquer_coisa = 'Teste'
---
+++ typescript/typescript/app3.ts
@@ -0,0 +1,10 @@
+import Carro from './Carro'
+import Moto from "./Moto";
+
+let carro = new Carro('Veloster', 3);
+
+let moto = new Moto();
+moto.acelerar();
+
+console.log(moto)
+console.log(carro)
