6dc6ccc0f0a6accf1bb68a3de32222406436bd3d
Tiago José Valdrich <tiago.valdrich@hotmail.com>
2018-07-02 22:37:30 -0300

    Creating the phrase verification,and making the progress bar be dynamics

--- projects/app1/src/app/painel/painel.component.ts
+++ projects/app1/src/app/painel/painel.component.ts
@@ -14,7 +14,14 @@ export class PainelComponent implements OnInit {
   public instrucao: string = "Traduza a frase:"
   public resposta: string
 
-  constructor() {  }
+  public rodada: number = 0
+  public rodadaFrase: Frase
+
+  public progresso: number = 0
+
+  constructor() { 
+    this.rodadaFrase = this.frases[this.rodada]
+   }
 
   ngOnInit() {
   }
@@ -23,7 +30,18 @@ export class PainelComponent implements OnInit {
     //console.log(resposta)
     //OU console.log((<HTMLInputElement>resposta.target).value)
     this.resposta = resposta
-    console.log(this.resposta)
+    //console.log(this.resposta)
+  }
+
+  public verificarResposta(): void {
+    
+    if(this.rodadaFrase.frasePtBr == this.resposta){
+      this.rodada++
+      this.progresso += (100/this.frases.length)
+      this.rodadaFrase = this.frases[this.rodada]
+    }else{
+      alert("Resposta errada !")
+    }
   }
 
 }
