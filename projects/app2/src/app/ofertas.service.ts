0742aa1ba2f576845fc90c11a337b56fbcfdeb23
Tiago José Valdrich <tiago.valdrich@hotmail.com>
2018-07-15 11:15:46 -0300

    Creating the ComoFazComponent and OndeFicaComponent, and adjusting the router for these elements.

--- projects/app2/src/app/ofertas.service.ts
+++ projects/app2/src/app/ofertas.service.ts
@@ -1,6 +1,8 @@
 import { Http } from '@angular/http'
 import { Injectable } from '@angular/core'
 import { Oferta } from './shared/oferta.model';
+import { URL_API } from './app.api';
+
 
 @Injectable()
 export class OfertasService {
@@ -8,14 +10,38 @@ export class OfertasService {
     constructor(private http: Http) {}   
 
     public getOfertas(): Promise<any> {
-        return this.http.get('http://localhost:3000/ofertas?destaque=true')
+        return this.http.get(`${URL_API}/ofertas?destaque=true`)
             .toPromise()
             .then((resposta: any) => resposta.json())
     }
 
     public getOfertasPorCategoria(categoria: string): Promise<any> {
-        return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+        return this.http.get(`${URL_API}/ofertas?categoria=${categoria}`)
             .toPromise()
             .then((response: any) => response.json())
+    }
+
+    public getOfertaById(id: number): Promise<any> {
+        return this.http.get(`${URL_API}/ofertas?id=${id}`)
+            .toPromise()
+            .then((response) => {
+                return response.json()[0]
+            })
+    }
+
+    public getComoUsarOfertaById(id: number): Promise<any> {
+        return this.http.get(`${URL_API}/como-usar?id=${id}`)
+            .toPromise()
+            .then((response: any) => {
+                return response.json().shift().descricao
+            })
+    }
+
+    public getOndeFicaOfertaById(id: number): Promise<any> {
+        return this.http.get(`${URL_API}/onde-fica?id=${id}`)
+            .toPromise()
+            .then((response: any) => {
+                return response.json().shift().descricao
+            })
     }
 }
