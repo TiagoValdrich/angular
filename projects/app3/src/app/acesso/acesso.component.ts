e86978de2c7fa94483047a736559bf9f46deda7e
Tiago José Valdrich <tiago.valdrich@hotmail.com>
2018-07-26 22:58:38 -0300

    Using EventEmitters and outputs for change the component showed in the right side of the screenn.

--- projects/app3/src/app/acesso/acesso.component.ts
+++ projects/app3/src/app/acesso/acesso.component.ts
@@ -33,9 +33,14 @@ export class AcessoComponent implements OnInit {
   public estadoBanner: string = 'criado'
   public estadoLogin: string = 'criado'
 
+  public cadastro: boolean = false
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  public exibirPainel(event: string): void {
+    this.cadastro = event == 'cadastro' ? true : false
+  }
 }
